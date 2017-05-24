68baf7799047e28cd77b56a4138ee719f1509d53
jrapedra <pedra.josericardo@gmail.com>
2017-05-24 09:46:16 +0100

    Formulário - Contacto

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	
 	public function __construct(){
-		parent::__construct();
+        parent::__construct();
+        $this->load->helper(array('form','url'));
+        $this->load->library(array('session', 'form_validation', 'email'));
 	}
 
 	public function index()
@@ -102,6 +104,68 @@ class Welcome extends CI_Controller {
 									'descricao'	=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.'
 									);
 		$this->load->view('init', $data);
+	}
+
+	public function contact()
+	{
+		$data['active_menu'] = 'contact';
+		$data['content'] = 'contact/index';
+		$data['css_files'] = ['styles.css'];
+
+        //set validation rules
+        $this->form_validation->set_rules('name', 'Nome', 'required');
+        $this->form_validation->set_rules('email', 'E-mail', 'required');
+        $this->form_validation->set_rules('subject', 'Assunto', 'required');
+        $this->form_validation->set_rules('message', 'Mensagem', 'required');
+
+        //run validation on form input
+        if ($this->form_validation->run() == FALSE)
+        {
+            //validation fails
+            $this->load->view('init', $data);
+        }
+        else
+        {
+            //get the form data
+            $name = $this->input->post('name');
+            $from_email = $this->input->post('email');
+            $subject = $this->input->post('subject');
+            $message = $this->input->post('message');
+
+            //set to_email id to which you want to receive mails
+            $to_email = 'guia.pratico.27@gmail.com';
+
+            //configure email settings
+            $config['protocol'] = 'smtp';
+            $config['smtp_host'] = 'ssl://smtp.gmail.com';
+            $config['smtp_port'] = '465';
+            $config['smtp_user'] = 'guia.pratic@gmail.com';
+            $config['smtp_pass'] = 'Qwe123!_';
+            $config['mailtype'] = 'html';
+            $config['charset'] = 'iso-8859-1';
+            $config['wordwrap'] = TRUE;
+            $config['newline'] = "\r\n"; //use double quotes
+            //$this->load->library('email', $config);
+            $this->email->initialize($config);                        
+
+            //send mail
+            $this->email->from($from_email, $name);
+            $this->email->to($to_email);
+            $this->email->subject($subject);
+            $this->email->message($message);
+            if ($this->email->send())
+            {
+                // mail sent
+                $this->session->set_flashdata('msg','<div class="alert alert-success text-center">Your mail has been sent successfully!</div>');
+                redirect('welcome/contact/index');
+            }
+            else
+            {
+                //error
+                $this->session->set_flashdata('msg','<div class="alert alert-danger text-center">There is error in sending mail! Please try again later</div>');
+                redirect('welcome/contact/index');
+            }
+        }
 	}
 
 }
---
+++ application/views/contact/index.php
@@ -0,0 +1,61 @@
+<br />
+<div class="container">
+    <form id="form-contact" class="form-horizontal" role="form" action="<?php base_url() ?>contact" method="POST">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 well">
+                <?php $attributes = array("class" => "form-horizontal", "name" => "contactform");
+                echo form_open("contactform/index", $attributes);?>
+                <fieldset>
+                <legend>Formulário de contacto</legend>
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <label for="name" class="control-label">Nome</label>
+                    </div>
+                    <div class="col-md-12">
+                        <input class="form-control" name="name" placeholder="O seu nome completo" type="text" value="<?php echo set_value('name'); ?>" />
+                        <span class="text-danger"><?php echo form_error('name'); ?></span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <label for="email" class="control-label">Email</label>
+                    </div>
+                    <div class="col-md-12">
+                        <input class="form-control" name="email" placeholder="O seu endereço de e-mail" type="text" value="<?php echo set_value('email'); ?>" />
+                        <span class="text-danger"><?php echo form_error('email'); ?></span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <label for="subject" class="control-label">Assunto</label>
+                    </div>
+                    <div class="col-md-12">
+                        <input class="form-control" name="subject" placeholder="O seu assunto" type="text" value="<?php echo set_value('subject'); ?>" />
+                        <span class="text-danger"><?php echo form_error('subject'); ?></span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <label for="message" class="control-label">Mensagem</label>
+                    </div>
+                    <div class="col-md-12">
+                        <textarea class="form-control" name="message" rows="4" placeholder="A sua mensagem"><?php echo set_value('message'); ?></textarea>
+                        <span class="text-danger"><?php echo form_error('message'); ?></span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input name="submit" type="submit" class="btn btn-primary" value="Enviar" />
+                    </div>
+                </div>
+                </fieldset>
+                <?php echo form_close(); ?>
+                <?php echo $this->session->flashdata('msg'); ?>
+            </div>
+        </div>
+    </form>
+</div>
